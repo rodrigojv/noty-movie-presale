33f96b67645262155608a3e655b330624822d860
Rodrigo Villalba <rodris.jvo@gmail.com>
2019-09-10 16:07:04 -0400

    Add background picture and other layout changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,18 +13,21 @@ function App() {
   };
 
   return (
-    <div className="bg-white rounded-t-lg overflow-hidden border-t border-l border-r border-gray-400 bg-gray-200 flex justify-center">
-      <div className="w-full max-w-xs">
-        <h1>Notificación de preventas para próximos estrenos de DC</h1>
-        <form
-          method="post"
-          className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
-        >
+    <div className="main-section bg-white rounded-t-lg overflow-hidden border-t border-l border-r border-gray-400 bg-gray-200 flex justify-center">
+      <div className="w-full max-w-4xl m-8">
+        <form method="post" className="px-8 pt-10 pb-10 mb-4">
+          <h1 className="title mt-8 text-gray-300 text-3xl">
+            Notificación de preventas para próximos estrenos de DC
+          </h1>
+          <h2 className="title mt-8 text-gray-300 text-xl">
+            Porque somos fanáticos de lo que se viene y no hay forma de saber
+            anticipidadmente en las apps de cines
+          </h2>
           <input type="hidden" name="form-name" value="contact" />
 
-          <div className="mb-4">
+          <div className="mb-4 mt-8">
             <label
-              className="block text-gray-700 text-sm font-bold mb-2"
+              className="block text-gray-300 text-sm font-bold mb-2"
               htmlFor="movie"
             >
               Película
@@ -32,6 +35,7 @@ function App() {
             <select
               name="movie"
               id="movie"
+              className="w-full  py-2 px-3"
               // value={movie}
               // onChange={e => setMovie(e.target.value)}
             >
@@ -42,7 +46,7 @@ function App() {
           </div>
           <div className="mb-6">
             <label
-              className="block text-gray-700 text-sm font-bold mb-2"
+              className="block text-gray-300 text-sm font-bold mb-2"
               htmlFor="theater"
             >
               Sala de Cine
@@ -50,6 +54,7 @@ function App() {
             <select
               name="theater"
               id="theater"
+              className="w-full  py-2 px-3"
               // value={theater}
               // onChange={e => setTheater(e.target.value)}
             >
@@ -58,7 +63,7 @@ function App() {
           </div>
           <div className="mb-4">
             <label
-              className="block text-gray-700 text-sm font-bold mb-2"
+              className="block text-gray-300 text-sm font-bold mb-2"
               htmlFor="mail"
             >
               Email
@@ -75,14 +80,14 @@ function App() {
           <div className="flex items-center justify-between">
             <button
               // onClick={onSignUp}
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              className="bg-blue-500 hover:bg-blue-700 text-gray-300 font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
               type="submit"
             >
               Sign Up
             </button>
           </div>
         </form>
-        <p className="text-center text-gray-500 text-xs">
+        <p className="text-center px-8 mt-8 mb-8 text-gray-500 text-xs">
           &copy;2019 Acme Corp. All rights reserved.
         </p>
       </div>
